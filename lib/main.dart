043da9c9d0f5e3efbb09d9a9753da23849c55deb
Ephraim <ephraim1213@yahoo.com>
2025-05-07 20:40:36 -0600

    Still overwriting code from demo, adjusting since its similar

--- lib/main.dart
+++ lib/main.dart
@@ -30,19 +30,23 @@ class MealPickerPage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MealPickerPage> createState() => _MealPickerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MealPickerState extends State<MealPickerPage> {
+  final List<String> meals = [
+    'Menudo',
+    'Posole',
+    'Asada Tacos',
+    'Navajo Tacos',
+    'Buffalo Wings',
+    'Steak'
+  ];
+  int currentMeal = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
     });
   }
