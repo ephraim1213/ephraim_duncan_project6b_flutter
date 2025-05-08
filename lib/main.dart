fce000dfed54ff3885e3426a6fb19044830cc876
Ephraim <ephraim1213@yahoo.com>
2025-05-07 20:57:46 -0600

    Final commit going to keep as simple as I can

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,11 @@ class MealPickerPage extends StatefulWidget {
   const MealPickerPage({super.key});
 
 
-  final String title;
-
   @override
   State<MealPickerPage> createState() => _MealPickerPageState();
 }
 
-class _MealPickerState extends State<MealPickerPage> {
+class _MealPickerPageState extends State<MealPickerPage> {
   final List<String> meals = [
     'Menudo',
     'Posole',
@@ -44,62 +42,34 @@ class _MealPickerState extends State<MealPickerPage> {
   ];
   int currentMeal = 0;
 
-  void _incrementCounter() {
+  void pickRandomMeal() {
     setState(() {
-
-      _counter++;
+      currentMeal = Random().nextInt(meals.length);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        title: const Text('Meal Selector'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              meals[currentMeal],
+              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: pickRandomMeal,
+              child: const Text('What to eat today'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
